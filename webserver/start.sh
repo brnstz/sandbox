df111b18a10ecc9ca6ba618a823cc3bb82f5b6b4
Brian Seitz <brnstz@gmail.com>
2013-05-13 21:30:03 -0400

    - changing to goserver instead of main

---
+++ webserver/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+go build goserver.go
+./goserver &
+echo $! > /var/run/goserver.pid
