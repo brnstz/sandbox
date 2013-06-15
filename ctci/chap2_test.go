747de722b30b1717aa82ed1bba45ea39b5703d56
Brian Seitz <brnstz@gmail.com>
2013-06-15 19:58:57 -0400

    - finished question 2.2

--- ctci/chap2_test.go
+++ ctci/chap2_test.go
@@ -39,7 +39,7 @@ func (n *node) String() string {
 	return str
 }
 
-// quesiton 2.1
+// question 2.1
 func removeDuplicates(n *node) {
 	dupes := map[interface{}]bool{}
 
@@ -67,6 +67,34 @@ func removeDuplicates(n *node) {
 	}
 }
 
+// question 2.2
+func findKthToLast(n *node, k int) interface{} {
+	nCount := n
+
+	i := 0
+	for {
+
+		if nCount == nil {
+			break
+		}
+
+		nCount = nCount.next
+		i++
+	}
+
+	// k is longer than i or k is negative, no element to be found
+	if k > i || k < 0 {
+		return nil
+	}
+
+	for j := 0; j < i-k-1; j++ {
+		fmt.Println("j: ", j)
+		n = n.next
+	}
+
+	return n.data
+}
+
 func CreateNodesFromString(data string, i, max int) *node {
 	if i < max {
 		curN := NewNode(data[i : i+1])
@@ -85,4 +113,20 @@ func TestRemoveDuplicates(t *testing.T) {
 	if node.String() != "FOLW UP" {
 		t.Error("remove duplicates did not match FOLW UP")
 	}
+}
+
+func TestFindKthToLast(t *testing.T) {
+	data := "ZHIS IS A STRING"
+	node := CreateNodesFromString(data, 0, len(data))
+
+	item := findKthToLast(node, 15)
+	if item != "Z" {
+		t.Error("15th node from end should be Z")
+	}
+
+	item = findKthToLast(node, -1)
+	if item != nil {
+		t.Error("-1th node from end should be nil")
+	}
+
 }
