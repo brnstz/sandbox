1ec8b230e6b2e32a6d49b1b8670d6e30074ac3ab
Brian Seitz <brnstz@gmail.com>
2014-04-07 18:42:59 -0400

    - adding dana redir

--- webserver/goserver.go
+++ webserver/goserver.go
@@ -4,7 +4,19 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+
+	"strings"
 )
+
+func measurementsRedir(w http.ResponseWriter, r *http.Request) {
+	lowerPath := strings.ToLower(r.URL.Path)
+
+	if strings.HasPrefix(lowerPath, "/nodana") {
+		http.Redirect(w, r, "https://www.youtube.com/watch?v=CcRk-zdn8Ps", http.StatusFound)
+	} else {
+		http.Redirect(w, r, "http://themeasurements.bandcamp.com/", http.StatusFound)
+	}
+}
 
 func main() {
 
@@ -12,16 +24,12 @@ func main() {
 	http.Handle("wholok.com/", http.FileServer(http.Dir("/home/bseitz/www/wholok.com")))
 	http.Handle("www.brnstz.com/", http.FileServer(http.Dir("/home/bseitz/www/brnstz.com")))
 	http.Handle("brnstz.com/", http.FileServer(http.Dir("/home/bseitz/www/brnstz.com")))
+
 	http.Handle("themeasurements.com/logo/", http.StripPrefix("/logo/", http.FileServer(http.Dir("/home/bseitz/www/themeasurements.com/logo/"))))
 	http.Handle("www.themeasurements.com/logo/", http.StripPrefix("/logo/", http.FileServer(http.Dir("/home/bseitz/www/themeasurements.com/logo/"))))
-	http.HandleFunc("themeasurements.com/",
-		func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "http://themeasurements.bandcamp.com/", http.StatusFound)
-		})
-	http.HandleFunc("www.themeasurements.com/",
-		func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "http://themeasurements.bandcamp.com/", http.StatusFound)
-		})
+
+	http.HandleFunc("themeasurements.com/", measurementsRedir)
+	http.HandleFunc("www.themeasurements.com/", measurementsRedir)
 
 	highlit_url, err := url.Parse("http://localhost:8080")
 	if err != nil {
