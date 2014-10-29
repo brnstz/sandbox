dc16d8579ecb448762dd5a02f5c95243e8f1d9c9
Brian Seitz <brnstz@gmail.com>
2014-10-29 00:52:02 -0400

    loading all of gp and wburg

--- drink/drink.go
+++ drink/drink.go
@@ -10,8 +10,11 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
 const (
@@ -41,9 +44,11 @@ const (
                 });
 
                 $.getJSON("svc/drinks", function(data) {
-                    for (i = 0; i < data.length; i++) {
-                        var marker = L.marker([data[i].lat, data[i].lon]).addTo(map);
-                        marker.bindPopup(data[i].licensee_name_business);
+                    for (var key in data) {
+                        console.log(key);
+                        console.log(data[key]);
+                        var marker = L.marker([data[key].lat, data[key].lon]).addTo(map);
+                        marker.bindPopup(data[key].licensee_name_business);
                     }
                 });
            });
@@ -58,8 +63,14 @@ var (
 	// URL to search for address and retrieve lat, lon
 	nomURL = `http://nominatim.openstreetmap.org/search`
 
-	// cache licenses after loading once
-	cachedLicenses []*license
+	// cache licenses after loading once, also allow to look up my
+	// license.key()
+	cachedLicenses = map[string]*license{}
+
+	// AVE or ST at end of string or word, case insensitive, optionally
+	// followed by .
+	aveR = regexp.MustCompile(`(?i)\sAVE\.?(?:\s|$)`)
+	stR  = regexp.MustCompile(`(?i)\sST\.?(?:\s|$)`)
 )
 
 // initValues is returned to init the map
@@ -92,6 +103,39 @@ type license struct {
 	// appended data from nominatim
 	Lat float64 `json:"lat"`
 	Lon float64 `json:"lon"`
+
+	// appended from restaurant inspection
+	CurrentGrade   string     `json:"currentgrade"`
+	InspectionDate *time.Time `json:"inspdate"`
+}
+
+// key returns a slightly normalized address to match the license to other
+// datasets
+func (l *license) key() string {
+	// The license dataset has mixed case and "AVE" or "ST", while the
+	// restaurant inspection dataset consistently has "AVENUE" and "STREET" in
+	// all caps. Do some regexs to normalize. This should match
+	// us.states.ny.cities.nyc.dohmh.restaurants.inspections building+street
+	a := l.Address
+
+	a = strings.TrimSpace(aveR.ReplaceAllString(a, " AVENUE "))
+	a = strings.TrimSpace(stR.ReplaceAllString(a, " STREET "))
+
+	return a
+}
+
+// inspectionResponse is what calls to
+// us.states.ny.cities.nyc.dohmh.restaurants.inspections return
+type inspectionResponse struct {
+	Result []*inspection
+}
+
+// inspection is a single result from inspectionResponse
+type inspection struct {
+	Building       string     `json:"building"`
+	Street         string     `json:"street"`
+	CurrentGrade   string     `json:"currentgrade"`
+	InspectionDate *time.Time `json:"inspdate"`
 }
 
 // nomResp is the response from http://nominatim.openstreetmap.org/
@@ -100,7 +144,7 @@ type nomResp []struct {
 	Lon string
 }
 
-// A single
+// enigmaSearchValue is a list of values for this key (column)
 type enigmaSearchVal struct {
 	key    string
 	values []string
@@ -147,12 +191,16 @@ func (esv *enigmaSearchVal) String() string {
 
 // getEnigmaURL returns a full URL to search dataset for the values passed.
 // Lets API infer default conjunction of "and". FIXME: May break in edge cases.
-func getEnigmaURL(dataset string, search []enigmaSearchVal, page string) string {
+func getEnigmaURL(dataset string, search []enigmaSearchVal, page, sort string) string {
 	v := url.Values{}
 	for _, s := range search {
 		v.Add("search[]", s.String())
 	}
 	v.Set("page", page)
+
+	if len(sort) > 0 {
+		v.Set("sort", sort)
+	}
 
 	baseURL := fmt.Sprint(
 		`https://api.enigma.io/v2/data/`, os.Getenv("ENIGMA_API_KEY"), `/`,
@@ -188,39 +236,50 @@ func getJSON(url string, obj interface{}) (err error) {
 	return
 }
 
-// appendGeo appends latitude and longitude to lic
-func appendGeo(lic *license) (err error) {
-	v := url.Values{}
-	v.Set("format", "json")
-	v.Set("street", lic.Address)
-	v.Set("city", lic.City)
-	v.Set("state", lic.State)
-
-	nr := nomResp{}
-	err = getJSON(fmt.Sprint(nomURL, "?", v.Encode()), &nr)
-	if err != nil {
-		log.Println(err)
-		return
+// saveLic serializes access to cachedLicenses
+func saveLic(licChan chan *license) {
+	for lic := range licChan {
+		log.Println(lic)
+		cachedLicenses[lic.key()] = lic
 	}
-	if len(nr) < 1 {
-		log.Println("No response from ", nomURL)
+}
 
-		return
+// processGeo receive licenses from a channel and retrieves the lat and lon
+// from nomURL
+func processGeo(geoChan, licChan chan *license, wg *sync.WaitGroup) {
+	for lic := range geoChan {
+		v := url.Values{}
+		v.Set("format", "json")
+		v.Set("street", lic.Address)
+		v.Set("city", lic.City)
+		v.Set("state", lic.State)
+
+		nr := nomResp{}
+		err := getJSON(fmt.Sprint(nomURL, "?", v.Encode()), &nr)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if len(nr) < 1 {
+			log.Println("No response from ", nomURL)
+			continue
+		}
+
+		lic.Lat, err = strconv.ParseFloat(nr[0].Lat, 64)
+		if err != nil {
+			log.Println("Can't convert latitude", err)
+			continue
+		}
+
+		lic.Lon, err = strconv.ParseFloat(nr[0].Lon, 64)
+		if err != nil {
+			log.Println("Can't convert longitude", err)
+			continue
+		}
+
+		licChan <- lic
 	}
-
-	lic.Lat, err = strconv.ParseFloat(nr[0].Lat, 64)
-	if err != nil {
-		log.Println("Can't convert latitude", err)
-		return
-	}
-
-	lic.Lon, err = strconv.ParseFloat(nr[0].Lon, 64)
-	if err != nil {
-		log.Println("Can't convert longitude", err)
-		return
-	}
-
-	return
+	wg.Done()
 }
 
 // loadDrinks loads places to drink in the background
@@ -231,31 +290,94 @@ func loadDrinks() {
 	// should be enough.
 	esv := enigmaSearchVal{
 		key:    "establishment_address_zip",
-		values: []string{"11222"},
+		values: []string{"11222", "11211"},
 	}
 	enigmaURL := getEnigmaURL(
-		`enigma.licenses.liquor.us`, []enigmaSearchVal{esv}, `1`,
+		`enigma.licenses.liquor.us`, []enigmaSearchVal{esv}, `1`, ``,
 	)
 
-	// Get the liceneses from Engima
+	// Get the licenses from Engima
 	eResp := licenseResponse{}
 	err := getJSON(enigmaURL, &eResp)
 	if err != nil {
-		log.Println(err)
 		return
+	}
+
+	geoChan := make(chan *license, 5000)
+	licChan := make(chan *license, 5000)
+
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go processGeo(geoChan, licChan, wg)
 	}
 
 	// Append geo info
 	for _, lic := range eResp.Result {
-		err = appendGeo(lic)
+		geoChan <- lic
+	}
+
+	// Close the input channel when we've added al
+	close(geoChan)
+
+	// Close the output channel once all processGeos have finished
+	wg.Wait()
+	close(licChan)
+
+	saveLic(licChan)
+
+	log.Println("done loading")
+}
+
+// loadInspections loads all inspections for 11222 and appends them to
+// licenses in cachedLicenses
+func loadInspections() {
+	esv := enigmaSearchVal{
+		key:    "zipcode",
+		values: []string{"11222", "11211"},
+	}
+
+	// Call enigma until no more results
+	pageInt := 1
+	for {
+		enigmaURL := getEnigmaURL(
+			`us.states.ny.cities.nyc.dohmh.restaurants.inspections`,
+			[]enigmaSearchVal{esv},
+			fmt.Sprint(pageInt),
+			// Sort by ascending date, so the final values are most recent
+			"inspdate+",
+		)
+
+		log.Println(enigmaURL)
+
+		iResp := inspectionResponse{}
+		err := getJSON(enigmaURL, &iResp)
 		if err != nil {
-			continue
+			return
 		}
 
-		log.Printf("%+v", lic)
-		cachedLicenses = append(cachedLicenses, lic)
+		// If no results, stop the loop
+		if len(iResp.Result) < 1 {
+			break
+		}
+
+		// Go over each result and update new values
+		for _, resp := range iResp.Result {
+			// The key is the addresss
+			key := fmt.Sprint(resp.Building, " ", resp.Street)
+
+			if cachedLicenses[key] != nil {
+				log.Printf("%+v", resp)
+				cachedLicenses[key].CurrentGrade = resp.CurrentGrade
+				cachedLicenses[key].InspectionDate = resp.InspectionDate
+			}
+		}
+
+		pageInt++
+
 	}
-	log.Println("done loading")
+
 }
 
 // initVals is a handler that returns JSON to initialize the map
@@ -301,7 +423,8 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	go loadDrinks()
+	loadDrinks()
+	loadInspections()
 
 	http.HandleFunc("/svc/init", initVals)
 	http.HandleFunc("/svc/drinks", drinks)
