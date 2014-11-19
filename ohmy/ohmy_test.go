d1967ae0fde0e1823ab915458b91c3bec3043d59
Brian Seitz <brnstz@gmail.com>
2014-11-19 13:37:11 -0500

    adding some random mongo stuff

--- ohmy/ohmy_test.go
+++ ohmy/ohmy_test.go
@@ -2,10 +2,24 @@ package ohmy_test
 
 import (
 	"github.com/brnstz/sandbox/ohmy"
+	"gopkg.in/mgo.v2"
 
 	"testing"
 )
 
 func TestOhMy(t *testing.T) {
 	ohmy.Doit()
+	s, err := mgo.Dial("192.168.59.103")
+	if err != nil {
+		panic(err)
+	}
+
+	c := s.DB("ohmy").C("bands")
+	band := ohmy.Band{
+		Name: "The Rolling Stones",
+	}
+	err = c.Insert(band)
+	if err != nil {
+		panic(err)
+	}
 }
