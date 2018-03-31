162931935b2e2d299e994476a496df072aa00562
Brian Seitz <brnstz@gmail.com>
2018-03-31 16:36:07 -0400

    mvp

--- video/video.go
+++ video/video.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"image/png"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
+	for true {
+		img, err := png.Decode(os.Stdin)
+		if err == io.EOF || err == io.ErrUnexpectedEOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(img.At(0, 0))
+	}
+
 }
